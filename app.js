71cdc450cd1359c01a6bc8c94b7ab952b0493cbd
Anirudh Raghunath <anraghun@github.com>
2020-08-13 16:29:06 +0530

    Another simple change

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World!!')
+  res.send('Hello World!! Bonjour!')
 })
 
 //Launch listening server on port 8080
 app.listen(8080, function () {
-  console.log('App listening on port 8080! Yay!')
+  console.log('App listening on port 8080!')
 })
